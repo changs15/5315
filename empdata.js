6c9d624d69c55f877fd2f14ae77644287b2c2102
stephanie <changs15@seattleu.edu>
2018-10-12 15:20:48 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -23,6 +23,94 @@
       "department": "Neurology",
       "job": "Registered Nurse",
       "location": "Main Hospital"
+    },
+    {
+      "name": "Harry Smith",
+      "phone_number": "(206)123-5432",
+      "email_address": "hsmith@hospital.com",
+      "department": "Internal Medicine",
+      "job": "Nursing Assistant",
+      "location": "Clinic"
+    },
+    {
+      "name": "Michael Higgins",
+      "phone_number": "(206)885-3456",
+      "email_address": "mhiggins@hospital.com",
+      "department": "Family Medicine",
+      "job": "Physician",
+      "location": "Main Hospital"
+    },
+    {
+      "name": "Victoria Moore",
+      "phone_number": "(206)882-6544",
+      "email_address": "vmoore@hospital.com",
+      "department": "Dermatology",
+      "job": "Physician",
+      "location": "Main Hospital"
+    },
+    {
+      "name": "Cary Gold",
+      "phone_number": "(206)882-4223",
+      "email_address": "cgold@hospital.com",
+      "department": "Neurology",
+      "job": "Surgeon",
+      "location": "Main Hospital"
+    },
+    {
+      "name": "Patrick Hoyt",
+      "phone_number": "(206)665-9483",
+      "email_address": "phoyt@hospital.com",
+      "department": "Psychiatry",
+      "job": "Psychiatrist",
+      "location": "Clinic"
+    },
+    {
+      "name": "Tim Peterson",
+      "phone_number": "(206)546-7284",
+      "email_address": "tpeterson@hospital.com",
+      "department": "Pharmacy",
+      "job": "Pharmacist",
+      "location": "Clinic"
+    },
+    {
+      "name": "Tracy Gomez",
+      "phone_number": "(206)873-3356",
+      "email_address": "tgomez@hospital.com",
+      "department": "Endocrinology",
+      "job": "Medical Assistant",
+      "location": "Main Hospital"
+    },
+    {
+      "name": "Samantha Finn",
+      "phone_number": "(206)279-6194",
+      "email_address": "sfinn@hospital.com",
+      "department": "Neurology",
+      "job": "Physician",
+      "location": "Main Hospital"
+    },
+    {
+      "name": "Reese Coleman",
+      "phone_number": "(206)946-2743",
+      "email_address": "rcoleman@hospital.com",
+      "department": "Dermatology",
+      "job": "Registered Nurse",
+      "location": "Clinic"
+    },
+    {
+      "name": "Trent Price",
+      "phone_number": "(206)390-7183",
+      "email_address": "tprice@hospital.com",
+      "department": "Family Medicine",
+      "job": "Medical Assistant",
+      "location": "Clinic"
+    },
+    {
+      "name": "Alicia Gregson",
+      "phone_number": "(206)431-8859",
+      "email_address": "agregson@hospital.com",
+      "department": "Internal Medicine",
+      "job": "Physician",
+      "location": "Clinic"
     }
   ]
 }
