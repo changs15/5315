306019e27d09307d77fc0c186f131d89cfc4ef7b
stephanie <changs15@seattleu.edu>
2018-10-12 15:55:40 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,7 +1,7 @@
 {
   "data": [
     {
-      "name": "Beth Simpson",
+      "hospital_employee": "Beth Simpson",
       "phone_number": "(206)886-1234",
       "email_address": "bsimpson@hospital.com",
       "department": "Cardiology",
@@ -9,7 +9,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "John Noel",
+      "hospital_employee": "John Noel",
       "phone_number": "(206)884-5678",
       "email_address": "jnoel@hospital.com",
       "department": "Cardiology",
@@ -17,7 +17,7 @@
       "location": "Main Hospital"
     }, 
     {
-      "name": "Chloe Holmes",
+      "hospital_employee": "Chloe Holmes",
       "phone_number": "(206)755-9876",
       "email_address": "cholmes@hospital.com",
       "department": "Neurology",
@@ -25,7 +25,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Harry Smith",
+      "hospital_employee": "Harry Smith",
       "phone_number": "(206)123-5432",
       "email_address": "hsmith@hospital.com",
       "department": "Internal Medicine",
@@ -33,7 +33,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Michael Higgins",
+      "hospital_employee": "Michael Higgins",
       "phone_number": "(206)885-3456",
       "email_address": "mhiggins@hospital.com",
       "department": "Family Medicine",
@@ -41,7 +41,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Victoria Moore",
+      "hospital_employee": "Victoria Moore",
       "phone_number": "(206)882-6544",
       "email_address": "vmoore@hospital.com",
       "department": "Dermatology",
@@ -49,7 +49,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Cary Gold",
+      "hospital_employee": "Cary Gold",
       "phone_number": "(206)882-4223",
       "email_address": "cgold@hospital.com",
       "department": "Neurology",
@@ -57,7 +57,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Patrick Hoyt",
+      "hospital_employee": "Patrick Hoyt",
       "phone_number": "(206)665-9483",
       "email_address": "phoyt@hospital.com",
       "department": "Psychiatry",
@@ -65,7 +65,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Tim Peterson",
+      "hospital_employee": "Tim Peterson",
       "phone_number": "(206)546-7284",
       "email_address": "tpeterson@hospital.com",
       "department": "Pharmacy",
@@ -73,7 +73,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Tracy Gomez",
+      "hospital_employee": "Tracy Gomez",
       "phone_number": "(206)873-3356",
       "email_address": "tgomez@hospital.com",
       "department": "Endocrinology",
@@ -81,7 +81,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Samantha Finn",
+      "hospital_employee": "Samantha Finn",
       "phone_number": "(206)279-6194",
       "email_address": "sfinn@hospital.com",
       "department": "Neurology",
@@ -89,7 +89,7 @@
       "location": "Main Hospital"
     },
     {
-      "name": "Reese Coleman",
+      "hospital_employee": "Reese Coleman",
       "phone_number": "(206)946-2743",
       "email_address": "rcoleman@hospital.com",
       "department": "Dermatology",
@@ -97,7 +97,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Trent Price",
+      "hospital_employee": "Trent Price",
       "phone_number": "(206)390-7183",
       "email_address": "tprice@hospital.com",
       "department": "Family Medicine",
@@ -105,7 +105,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Alicia Gregson",
+      "hospital_employee": "Alicia Gregson",
       "phone_number": "(206)431-8859",
       "email_address": "agregson@hospital.com",
       "department": "Internal Medicine",
@@ -113,7 +113,7 @@
       "location": "Clinic"
     },
     {
-      "name": "Melissa Walker",
+      "hospital_employee": "Melissa Walker",
       "phone_number": "(206)859-3641",
       "email_address": "mwalker@hospital.com",
       "department": "Cardiology",
